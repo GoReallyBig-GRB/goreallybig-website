40a5afd45ff4b100d37caecbc1013ca95dcc4376
GoReallyBig <goreallybig.business@gmail.com>
2026-09-13 00:52:37 +0100

    Inject reliability client

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,3 +1,7 @@
 export async function onRequest(context) {
-  return context.next();
+  const response = await context.next();
+  const type = response.headers.get('content-type') || '';
+  if (!type.includes('text/html')) return response;
+  const html = await response.text();
+  return new Response(html.replace('</body>', '<script src="/assets/proxy-client.js" defer></script></body>'), response);
 }
