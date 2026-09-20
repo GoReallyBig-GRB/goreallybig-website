43ff61c6bcdbad56fc0971457e4cab684f09011c
GoReallyBig <goreallybig.business@gmail.com>
2026-09-20 11:50:55 +0100

    Route lead submissions through Cloudflare Worker

--- _worker.js
+++ _worker.js
@@ -1,5 +1,19 @@
+import { onRequest as submitLead } from './functions/api/submit-lead.js';
+
 export default {
-  async fetch(request, env) {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/api/submit-lead') {
+      return submitLead({
+        request,
+        env,
+        ctx,
+        waitUntil: (promise) => ctx.waitUntil(promise),
+        next: () => env.ASSETS.fetch(request),
+      });
+    }
+
     return env.ASSETS.fetch(request);
   }
 };
